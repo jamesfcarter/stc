ce87844dfd4042553f4cd17cb01367317b023e9a
James F. Carter <james@jfc.org.uk>
2016-09-03 16:15:02 +0100

    removed unused function

--- index.go
+++ index.go
@@ -24,14 +24,6 @@ type Index struct {
 	Entries map[string][]IndexItem
 }
 
-func AToZ() []string {
-	return []string{
-		"0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
-		"L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
-		"X", "Y", "Z",
-	}
-}
-
 func (stc *Stc) LoadIndices() error {
 	var err error
 
